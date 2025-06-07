08fcbe3762e952c10842bb857d7499d68286a905
Maximiliano Pomar <maxipomar@gmailcom>
2025-06-07 20:02:37 -0300

    [LVL-1]: Creates GetMessage function

---
+++ src/main/java/com/quasar/service/MessageBuilder.java
@@ -0,0 +1,25 @@
+package com.quasar.service;
+
+import com.quasar.exception.InvalidMessageLengthException;
+
+public class MessageBuilder {
+
+    public String GetMessage(String[][] messages) {
+        int messageLength = messages[0].length;
+        String[] completeMessage = new String[messageLength];
+        for (String[] words : messages) {
+            for (int i = 0; i < messageLength; i++) {
+                if (words.length != messageLength) {
+                    throw new InvalidMessageLengthException("Los mensajes no tienen la misma longitud");
+                }
+                String word = words[i];
+                String lastWord = completeMessage[i];
+                Boolean lastWordWasEmpty = "".equals(lastWord) || lastWord == null;
+                if (lastWordWasEmpty) {
+                    completeMessage[i] = word;
+                }
+            }
+        }
+        return String.join(" ", completeMessage);
+    }
+}
